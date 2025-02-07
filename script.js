223ae746c670905b2be773233b55d56ebb283ab3
Ukval <ukwitegetsev9@gamail.com>
2025-02-07 19:07:43 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,38 @@
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
+
+function opentab(tabname) {
+    for (let tablink of tablinks) {
+        tablink.classList.remove("active-link");
+    }
+    
+    for (let tabcontent of tabcontents) {
+        tabcontent.style.display = "none";  
+    }
+    document.getElementById(tabname).style.display = "block";
+    event.currentTarget.classList.add("active-link");  
+}
+
+fetch('https://script.google.com/macros/s/AKfycbx7fekzQcaHiQQJGrTCvR9TLEHRSJVIbW8U4vbepG0HpxsKlPxM7fDfwo8-trLqn00g/exec', {
+    method: 'POST',
+    body: formData,
+})
+.then(response => response.json())
+.then(data => {
+    alert(data.result);
+})
+.catch(error => console.error('Error:', error));
+
+document.getElementById('form').addEventListener('submit', function(e) {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    fetch('https://script.google.com/macros/s/AKfycbx7fekzQcaHiQQJGrTCvR9TLEHRSJVIbW8U4vbepG0HpxsKlPxM7fDfwo8-trLqn00g/exec', { // Ensure this URL is correct
+        method: 'POST',
+        body: formData,
+    })
+    .then(response => response.json())
+    .then(data => {
+        alert(data.result);
+    })
+    .catch(error => console.error('Error:', error));
+});
